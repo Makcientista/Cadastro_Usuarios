99e4bc97941a762c791bc06e300ade68aa7841ed
makiesse <ddociel@hotmail.com>
2023-03-05 21:50:11 -0300

    Changing the Sign up and login form image

--- src/main/java/com/makiesse/registration/RegistrationServlet.java
+++ src/main/java/com/makiesse/registration/RegistrationServlet.java
@@ -1,6 +1,12 @@
 package com.makiesse.registration;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,13 +16,50 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class RegistrationServlet
  */
-@WebServlet("/RegistrationServlet")
+@WebServlet("/register")
 public class RegistrationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
           
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {		
+				
+		
+		/* Get de information enter by the user in "registration form"*/
+		String username = request.getParameter("name");
+		String useremail = request.getParameter("email");
+		String userpassword = request.getParameter("pass");
+		String usermobile = request.getParameter("contact");
+		RequestDispatcher dispatcher = null;
+		Connection con = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/company");
+			PreparedStatement pst = con.prepareStatement("insert into users(username, useremail, userpassword, usermobile) values(?,?,?,?) ");
+			pst.setString(1, username);
+			pst.setString(2, useremail);
+			pst.setString(3, userpassword);
+			pst.setString(4, usermobile);
+			
+			int rowCount = pst.executeUpdate();
+			dispatcher = request.getRequestDispatcher("registration.jsp");
+			
+			if (rowCount > 0) {
+				request.setAttribute("status", "sucesso");			
+				
+			}else {
+				request.setAttribute("status", "negado");
+			}
+			dispatcher.forward(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
-
+	
 }
